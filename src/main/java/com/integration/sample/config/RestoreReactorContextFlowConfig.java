22e2adeb9d655dcdfc919ac4b8791e085a25f0b8
syedyusufh <54112364+syedyusufh@users.noreply.github.com>
2024-06-21 20:32:33 +0400

    custom webclient logger

--- src/main/java/com/integration/sample/config/RestoreReactorContextFlowConfig.java
+++ src/main/java/com/integration/sample/config/RestoreReactorContextFlowConfig.java
@@ -4,7 +4,6 @@ import static java.time.Duration.ofMinutes;
 import static org.springframework.integration.context.IntegrationContextUtils.NULL_CHANNEL_BEAN_NAME;
 import static org.springframework.integration.dsl.Pollers.fixedDelay;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -18,18 +17,15 @@ public class RestoreReactorContextFlowConfig {
 
 	private static final String ECHO_GET = "https://echo.free.beeceptor.com/sample-request?author=beeceptor";
 
-	@Autowired
-	private WebClient.Builder webClientBuilder;
-
 	@Bean
-	public IntegrationFlow restoreReactorContextFlow() {
+	public IntegrationFlow restoreReactorContextFlow(WebClient webClient) {
 
 		// @formatter:off
 		return IntegrationFlow.fromSupplier(() -> "Good Morning", ec -> ec.poller(fixedDelay(ofMinutes(1))))
 							.enrichHeaders(hdrSpec -> hdrSpec.headerFunction("INPUT_GREETING", m -> m.getPayload()))
 							.log(LoggingHandler.Level.INFO, m -> "----------------------START-------------------------")
 							.log(LoggingHandler.Level.INFO, m -> "WebClient GET Request (start of reactorContext)")
-							.handle(WebFlux.outboundGateway(ECHO_GET, webClient())
+							.handle(WebFlux.outboundGateway(ECHO_GET, webClient)
 										.httpMethod(HttpMethod.GET)
 										.expectedResponseType(String.class),
 									ec -> ec.customizeMonoReply((message, mono) -> mono.contextCapture()))
@@ -38,11 +34,6 @@ public class RestoreReactorContextFlowConfig {
 							.channel(NULL_CHANNEL_BEAN_NAME)
 							.get();
 		// @formatter:on
-	}
-
-	@Bean
-	public WebClient webClient() {
-		return webClientBuilder.build();
 	}
 
 }
